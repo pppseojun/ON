84f6d869226b35e279f16294b406b71cfed9e30d
pppseojun <psjprog1225@gmail.com>
2023-06-23 09:57:09 +0900

    수정
    수정

--- noplastic/script.js
+++ noplastic/script.js
@@ -14,3 +14,13 @@ document.getElementById("scroll-to-top").addEventListener("click", function() {
     behavior: "smooth"
   });
 });
+
+// Company이것을 클릭하면 sub_menu가 보이고 다시 Company클릭하면 숨겨지게 코드
+function toggleSubMenu() {
+  var subMenu = document.getElementById("sub_menu");
+  if (subMenu.style.display === "none") {
+    subMenu.style.display = "block";
+  } else {
+    subMenu.style.display = "none";
+  }
+}
